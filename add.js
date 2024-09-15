505be56f4eb29d435b106d7f15cd7fc41b8b9cc5
warren-glitch <warrenpiano24@gmail.com>
2024-09-15 14:27:40 +0800

    initial commit

---
+++ add.js
@@ -0,0 +1,19 @@
+// CONVERT STRING TO NUMBER
+let price = '200';
+price = parseInt(price), number(), +
+console.log(price);
+console.log(typeof price);
+
+// CONVERT NUMBER TO STRING
+let  amount = '165';
+amount = amount.toString();
+amount = string();
+console.log(amount);
+console.log(typeof amount):
+
+// CONVERT STRING TO DECIMAL
+let height = '155.5';
+height = parseFloat(height);
+console.log(height);
+console.log(typeof height);
+
